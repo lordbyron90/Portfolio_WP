2c60353e28cb48512c37ac3ef452b3cf4609d00d
Javi <github@snaven.com>
2017-08-31 23:57:34 +0200

    added footer scripts

--- wp-content/themes/nlp_theme/includes/footer.php
+++ wp-content/themes/nlp_theme/includes/footer.php
@@ -14,45 +14,10 @@
 
 			?>
  
-			<!-- <nav>
-				<ul class="ssmm_li">
-					<li class="i_instagram footer_li gradient_hover"><a href="/">instagram.</a></li>
-					<li class="i_behance footer_li gradient_hover"><a href="/">behance.</a></li>
-					<li class="i_spotify footer_li gradient_hover"><a href="/">spotify.</a></li>
-					<li class="i_twitter footer_li gradient_hover"><a href="/">twitter.</a></li>
-				</ul>
-			</nav> -->
 
 		</footer><!--FOOTER -->
 
 	</div>	
 
 </div><!--END OF FULL WRAPPER -->
-
-
-  	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-	<script src='<?php bloginfo('stylesheet_directory') ?>/js/isotope.3.js'></script>
-    <script src="<?php bloginfo('stylesheet_directory') ?>/js/index.js?"></script>
-    <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
-	    <script>
-      (function(){
-
-        var config = {
-          viewFactor : 0.15,
-          duration   : 800,
-          distance   : "0px",
-          reset: false,
-          scale      : 0.8,
-          mobile: true,
-          useDelay: 'always',
-          viewFactor: 0.1
-        }
-
-        window.sr = new ScrollReveal(config)
-	    sr.reveal('.item', { container: '.portfolio_container' });
-
-      })()
-
-    </script>
-</body>
-</html>
+<?php get_footer(); ?>
